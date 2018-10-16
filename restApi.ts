ea72c37e15b57512d7297fe8c1530f3a8d458ad3
vladbilenko <vlad.bilenko.se@gmail.com>
2018-10-16 19:20:16 +0300

    Add files

---
+++ restApi.ts
@@ -0,0 +1,79 @@
+import axios from 'axios';
+
+// tslint:disable:no-console
+
+interface IRequestObject {
+    link: string,
+    data?: any
+}
+
+import UserStore from '../stores/userStore';
+
+interface IRestApi {
+    get(requestConfig: IRequestObject): any,
+
+    post(requestConfig: IRequestObject): any,
+
+    privatePost(requestConfig: IRequestObject): any,
+
+    privateGet(requestConfig: IRequestObject): any
+}
+
+class API implements IRestApi {
+    private config = {
+        apiUrl: "https://api.proxyshop2018.tk"
+    };
+
+    public get = (requestConfig: IRequestObject) => {
+        return axios.get((this.config.apiUrl + requestConfig.link), {params: requestConfig.data});
+    };
+
+    public post = (requestConfig: IRequestObject) => {
+        return axios.post((this.config.apiUrl + requestConfig.link), requestConfig.data);
+    };
+
+    public privateGet = (requestConfig: IRequestObject) => {
+        return axios.get((this.config.apiUrl + requestConfig.link), {params: this.getApiKey});
+    };
+
+    public privatePost = (requestConfig: IRequestObject) => {
+            const response = axios.post((this.config.apiUrl + requestConfig.link), {...requestConfig.data, ...this.getApiKey});
+
+            // listen if user api key is not valid
+            response.then(
+                res => {
+                    if (res.data.error) {
+                        console.log(res.data.error);
+                        UserStore.forceUserLogout();
+                    }
+                }
+            );
+
+            return response;
+
+    };
+
+    public privatePostMultipart = (requestConfig: IRequestObject) => {
+        requestConfig.data.append('api', localStorage.getItem('apiKey') || '');
+
+        const response = axios.post((this.config.apiUrl + requestConfig.link), requestConfig.data, {headers: {'content-type': 'multipart/form-data'}});
+
+        // listen if user api key is not valid
+        response.then(
+            res => {
+                if (res.data.error) {
+                    console.log(res.data.error);
+                    UserStore.forceUserLogout();
+                }
+            }
+        );
+
+        return response;
+    };
+
+    private get getApiKey() {
+        return {api: localStorage.getItem('apiKey') || ''};
+    }
+}
+
+export const Api = new API();
